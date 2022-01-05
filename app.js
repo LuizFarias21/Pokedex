2484d337673a0840c34205a0bdd8159c49493164
Luiz Claudio <luizbfarias21@gmail.com>
2022-01-05 17:36:25 -0300

    update

--- app.js
+++ app.js
@@ -3,44 +3,39 @@ const content = document.querySelector('.content');
 const fetchPokemon = async () => {
     const getPokemonUrl = id => `https://pokeapi.co/api/v2/pokemon/${id}`;
 
-    const pokemonPromisses = [];
-
     for (let i = 1; i <= 151; i++) {
+
         const fetchData = await fetch(getPokemonUrl(i));
         const pokemonData = await fetchData.json();
-        pokemonPromisses.push(pokemonData);
 
         const container = document.createElement('div');
         container.className = 'container';
+
         const circle = document.createElement('div');
         circle.className = 'circle';
+
         const img = document.createElement('img');
         img.src = pokemonData.sprites.other['official-artwork'].front_default;
+
         const h2 = document.createElement('h2');
         h2.textContent = pokemonData.name;
+
         const types = document.createElement('div');
 
 
         content.append(container);
-
         container.append(circle);
         container.append(img);
         container.append(h2);
 
         for (let i = 0; i <= pokemonData.types.length - 1; i++) {
             const span = document.createElement('span');
-            span.textContent = `${pokemonData.types[i].type.name} `;
+            span.textContent = pokemonData.types[i].type.name;
+            span.className = pokemonData.types[i].type.name;
             types.append(span);
             container.append(types);
         }
-
     }
-
-    Promise.all(pokemonPromisses)
-        .then(pokemons => {
-            console.log(pokemons);
-        })
-
 }
 
 fetchPokemon();
